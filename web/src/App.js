27a5b191b8889d21de836706254f83baed7335ec
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-01-15 20:17:56 -0300

    conclusão aula 3

--- web/src/App.js
+++ web/src/App.js
@@ -1,94 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-
+import DevItem from './components/DevItem/index';
+import DevForm from './components/Devform/index';
 
 
 function App() {
+  const [devs, setDevs] = useState([]);
+ 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
 
-  const [latitude, setLatitude] = useState('');
-  const [lontitude, setLongitude] = useState('');
-
-  useEffect(() =>{
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-
-        setLatitude(latitude);
-        setLongitude(longitude);
-
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000, 
-      }
-    )
+    loadDevs();
   }, []);
 
+  async function handleAddDev(data){
+
+    const response = await api.post('/devs', data)
+
+    
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
-        <strong>Cadastrar:)</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number" 
-                name="latitude" 
-                id="latitude" 
-                required value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type="number" 
-                name="longitude" 
-                id="longitude" 
-                required value={lontitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/45343415?s=460&v=4" alt="Elbert Ribeiro"/>
-              <div className="user-info">
-                <strong>Elbert Ribeiro</strong>
-                <span>Python, Js, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Engenharia de Computação e Técnico em Informática.</p>
-            <a href="https://github.com/ElbertRibeiro">Acessar perfil no Github</a>
-
-          </li>
+          {devs.map( dev => (
+           <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
-
       </main>
     </div>
   );
