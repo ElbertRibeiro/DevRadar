40ce96c37342e5a7bb5dc9cc7ec15a26e4bd7229
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-01-19 13:35:52 -0300

    up

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect } from '../services/socket';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -36,6 +37,17 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    function setupWebsoket() {
+        const { latitude, longitude } = currentRegion;
+
+
+        connect(
+            latitude,
+            longitude,
+            techs,
+        );
+    }
+
     async function loadDevs() {
         const { latitude, longitude } = currentRegion;
 
@@ -48,6 +60,7 @@ function Main({ navigation }) {
         });
 
         setDevs(response.data.devs);
+        setupWebsoket();
 
     }
 
